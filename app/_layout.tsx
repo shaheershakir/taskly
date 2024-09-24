a889eb5b9a879daf8e17c3c09f282bccbac96636
shaheer shakir <shaheershakir22@gmail.com>
2024-09-24 16:02:15 +0530

    change nav to bottom tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,39 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import Feather from "@expo/vector-icons/Feather";
+import FeatherClock from "@expo/vector-icons/Feather";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import { theme } from "../theme";
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "Shopping list" }} />
-    </Stack>
+    <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorCerulean }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Shopping list",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="list" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="counter"
+        options={{
+          title: "counter",
+          tabBarIcon: ({ color, size }) => (
+            <FeatherClock name="clock" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="idea"
+        options={{
+          title: "Idea",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome6 name="lightbulb" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
---
+++ app/counter.tsx
@@ -0,0 +1,22 @@
+import { useRouter } from "expo-router";
+import { Text, View, StyleSheet } from "react-native";
+
+export default function CounterScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Counter</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  text: {
+    fontSize: 24,
+  },
+});
